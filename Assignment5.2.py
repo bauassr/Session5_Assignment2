f350a931f21287bd21ba5089d496872e6f633fe7
bauassr <bauassr@gmail.com>
2018-03-31 21:39:33 +0530

    Update Assignment5.2.py

--- Assignment5.2.py
+++ Assignment5.2.py
@@ -7,16 +7,27 @@ Created on Fri Mar 30 12:08:50 2018
 Subjects =["Americans","Indians"]
 Verbs=["Play","Watch"]
 Objects=["Baseball","Cricket"]
+class Sentence:
+    def __init__(self,Sub,Veb,Obj):
+        self.__Sub=Sub
+        self.__Veb=Veb
+        self.__Obj=Obj
+     
+    def __str__(self):
+        return "%s %s %s." \
+               %(self.__Sub,self.__Veb,self.__Obj)
+    
 
 def Make_sentance(Sub,Veb,ob):
     for s in Sub:
         for v in Veb:
             for o in ob :
-                print(s,v,o)
-                
+                Sen= Sentence(s,v,o)
+                print(Sen)
  
 Make_sentance(Subjects,Verbs,Objects)   
 
+    
     
     
     
